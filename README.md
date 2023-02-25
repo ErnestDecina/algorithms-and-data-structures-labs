b1df04b6762c4183d85895fe245d2bd359ec4502
Ernest Decina <ernestjohn007@gmail.com>
2023-02-25 12:02:53 +0000

    Initial Algorithms & Data Structures Lab's Repo

---
+++ README.md
@@ -0,0 +1,4 @@
+# CMPU2001 Algorithms & Data Structures Lab's
+
+All 2023 Algorithms & Data Structures Lab's
+
---
+++ java/src/com/ernestjohndecina/Main.java
@@ -0,0 +1,7 @@
+package com.ernestjohndecina;
+
+public class Main {
+    public static void main(String args[]) {
+        System.out.println("Hello World");
+    } // End void main
+} // End class Main
